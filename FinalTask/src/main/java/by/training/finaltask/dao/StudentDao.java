72d72eee65941f93487c9eeb5dac8c839c383d3c
Kynca <61618685+Kynca@users.noreply.github.com>
2021-10-17 14:09:01 +0300

    Delete FinalTask directory

--- FinalTask/src/main/java/by/training/finaltask/dao/StudentDao.java
+++
@@ -1,6 +0,0 @@
-package by.training.finaltask.dao;
-
-import by.training.finaltask.bean.entities.Student;
-
-public interface StudentDao extends Dao<Integer, Student> {
-}
--- FinalTask/src/main/java/by/training/finaltask/dao/UniversityDao.java
+++
@@ -1,6 +0,0 @@
-package by.training.finaltask.dao;
-
-import by.training.finaltask.bean.entities.University;
-
-public interface UniversityDao extends Dao<Integer, University> {
-}
